0c3b9ec20247ca62be1428ab62b09fca48d35a49
James Dam <jamesdam2005@gmail.com>
2025-06-27 17:22:48 +0000

    work on loading api into database

--- paper_trading.py
+++ paper_trading.py
@@ -1,6 +1,8 @@
 import json
 import os
+import pandas as pd
 import requests
+import sqlalchemy as db
 
 # Url of Financial Modeling Prep API
 url = 'https://financialmodelingprep.com/stable/search-symbol?'
@@ -16,5 +18,18 @@ params = {
 
 response = requests.get(url, params=params)
 
+# Turn json data into dict
 data = response.json()
+# Turn dict into DataFrame
+data = pd.DataFrame.from_dict(data)
+
+# Create engine object
+engine = db.create_engine('sqlite:///stocks.db')
+# Send SQLtable from data
+dataframe_name.to_sql('stocks', con=engine, if_exists='replace', index=False)
+
+# Print data and table
 print(data)
+with engine.connect() as connection:
+  query_result = connection.execute(db.text("SELECT * FROM stocks;")).fetchall()
+  print(pd.DataFrame(query_result))
