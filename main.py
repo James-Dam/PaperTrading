54b6d57ce7b25615af4861651d2dacb912027e7a
James Dam <jamesdam2005@gmail.com>
2025-06-28 06:51:09 +0000

    start modularizing, add workflows and gitignore

---
+++ main.py
@@ -0,0 +1,4 @@
+from paper_trading import run
+
+if __name__ == "__main__":
+    run()
--- paper_trading.py
+++ paper_trading.py
@@ -4,32 +4,33 @@ import pandas as pd
 import requests
 import sqlalchemy as db
 
-# Get user input for stock ticker symbol
-user_symbol = input("Enter a ticker symbol: ")
+def run():
+    # Get user input for stock ticker symbol
+    user_symbol = input("Enter a ticker symbol: ")
 
-# Url of Financial Modeling Prep API
-url = f'https://financialmodelingprep.com/api/v3/stock/full/real-time-price/{user_symbol}?'
+    # Url of Financial Modeling Prep API
+    url = f'https://financialmodelingprep.com/api/v3/stock/full/real-time-price/{user_symbol}?'
 
-# API key
-KEY = os.environ.get('FINANCIAL_MODELING_PREP_KEY')
+    # API key
+    KEY = os.environ.get('FINANCIAL_MODELING_PREP_KEY')
 
-# Test GET request of Financial Modeling Prep
-params = {
-  'apikey': KEY
-}
+    # Test GET request of Financial Modeling Prep
+    params = {
+    'apikey': KEY
+    }
 
-response = requests.get(url, params=params)
+    response = requests.get(url, params=params)
 
-data = response.json()
-data = pd.DataFrame.from_dict(data)
+    data = response.json()
+    data = pd.DataFrame.from_dict(data)
 
-engine = db.create_engine('sqlite:///stocks.db')
-# Send SQLtable from data
-data.to_sql('stocks', con=engine, if_exists='replace', index=False)
+    engine = db.create_engine('sqlite:///stocks.db')
+    # Send SQLtable from data
+    data.to_sql('stocks', con=engine, if_exists='replace', index=False)
 
-# Print data and table
-print(data)
+    # Print data and table
+    print(data)
 
-with engine.connect() as connection:
-  query_result = connection.execute(db.text("SELECT lastSalePrice FROM stocks;")).fetchall()
-  print(pd.DataFrame(query_result))
+    with engine.connect() as connection:
+        query_result = connection.execute(db.text("SELECT lastSalePrice FROM stocks;")).fetchall()
+        print(pd.DataFrame(query_result))
