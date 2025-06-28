60decf174ba6ea60b3268d697c8e3189caae293e
James Dam <jamesdam2005@gmail.com>
2025-06-28 06:24:47 +0000

    add user input for stock ticker symbol

--- paper_trading.py
+++ paper_trading.py
@@ -4,32 +4,32 @@ import pandas as pd
 import requests
 import sqlalchemy as db
 
+# Get user input for stock ticker symbol
+user_symbol = input("Enter a ticker symbol: ")
+
 # Url of Financial Modeling Prep API
-url = 'https://financialmodelingprep.com/stable/search-symbol?'
+url = f'https://financialmodelingprep.com/api/v3/stock/full/real-time-price/{user_symbol}?'
 
 # API key
 KEY = os.environ.get('FINANCIAL_MODELING_PREP_KEY')
 
 # Test GET request of Financial Modeling Prep
 params = {
-  'query': 'AAPL',
   'apikey': KEY
 }
 
 response = requests.get(url, params=params)
 
-# Turn json data into dict
 data = response.json()
-# Turn dict into DataFrame
 data = pd.DataFrame.from_dict(data)
 
-# Create engine object
 engine = db.create_engine('sqlite:///stocks.db')
 # Send SQLtable from data
-dataframe_name.to_sql('stocks', con=engine, if_exists='replace', index=False)
+data.to_sql('stocks', con=engine, if_exists='replace', index=False)
 
 # Print data and table
 print(data)
+
 with engine.connect() as connection:
-  query_result = connection.execute(db.text("SELECT * FROM stocks;")).fetchall()
+  query_result = connection.execute(db.text("SELECT lastSalePrice FROM stocks;")).fetchall()
   print(pd.DataFrame(query_result))
