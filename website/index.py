8a26c9b9e5268ea9ee6715b596f5585926996fed
James Dam <jamesdam2005@gmail.com>
2025-07-14 19:59:25 +0000

    work on registration

--- website/index.py
+++ website/index.py
@@ -1,13 +1,29 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+from flask_debugtoolbar import DebugToolbarExtension
+from flask import Flask, render_template, url_for, flash, redirect, request
+from forms import RegistrationForm
+from flask_behind_proxy import FlaskBehindProxy
 
-@app.route("/")
-def hello_world():
-    return render_template('home.html', subtitle='Home Page', text='This is the home page')
+app = Flask(__name__)
+toolbar = DebugToolbarExtension(app)
+proxied = FlaskBehindProxy(app)
+app.config['SECRET_KEY'] = 'b04464d4af1ddfaa05582eef16047910'
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    username = request.args.get('username', 'James')
+    return render_template('home.html', username=username)
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit(): # checks if entries are valid
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home', username=form.username.data)) # if so - send to home page
+    return render_template('register.html', title='Register', form=form)
     
-@app.route("/second_page")
-def second_page():
-    return render_template('second_page.html', subtitle='Second Page', text='This is the second page')
-    
+
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0", port=3000)
